74e340a81c475393035451fbef4eed5a60842dd2
davidcoats <david@getsafetycone.com>
2023-06-14 00:32:44 -0700

    Interim changes.

--- source/R5T.S0060.S002/Code/Program.cs
+++ source/R5T.S0060.S002/Code/Program.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Threading.Tasks;
+
+using CliWrap;
 
 using R5T.F0000;
 
@@ -7,9 +10,11 @@ namespace R5T.S0060.S002
 {
     class Program
     {
-        static void Main()
+        static async Task Main()
         {
             var writer = Console.Out;
+
+            await Cli.Wrap("ipconfig").ExecuteAsync();
 
             //InputArgumentOperator.Instance.DescribeTo(writer);
             InputArgumentOperator.Instance.DescribeTo_IncludingZeroth(writer);
